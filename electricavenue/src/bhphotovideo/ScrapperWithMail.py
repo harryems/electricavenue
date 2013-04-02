a1a1c385d41ab9bcf870928c4c5f7af66b9885a2
Carlos Espinosa de los Monteros Sanchez <carlos.espinosa@outlook.com>
2013-04-02 15:18:42 -0400

    inserting logs in scrapWithMail

--- electricavenue/src/bhphotovideo/ScrapperWithMail.py
+++ electricavenue/src/bhphotovideo/ScrapperWithMail.py
@@ -1,4 +1,3 @@
-
 import cookielib
 import urllib
 import urllib2
@@ -9,6 +8,7 @@ import csv
 import re
 from datetime import date
 from send_mail import send_mail
+import threading
 
 
 __author__ = 'Carlos Espinosa'
@@ -16,7 +16,7 @@ __author__ = 'Carlos Espinosa'
 class Spider:
     def __init__(self):
         self.opener = None
-        self.RETRY_COUNT = 5
+        self.RETRY_COUNT = 10
         self.USER_AGENT = ('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:14.0) Gecko/20100101 Firefox/14.0.1')
         self.headers = [self.USER_AGENT]
 
@@ -52,29 +52,33 @@ class Spider:
                 print 'Failed to fetch data after 5 retry.'
         return None
 
-class Main:
+class Main(threading.Thread):
     def __init__(self, url):
         self.spider = Spider()
         self.mainUrl = url
         self.referer = None
 
     def scrapItem(self, url,pgn=1):
+        threading.Thread.__init__(self)
         tmp =url.split('/')
         urlIntern=mainUrl+'/c/buy/'+tmp[5]+'/ipp/100/ci/'+tmp[7]+'/pn/'+str(pgn)+'/N/'+tmp[9]        
         headerInfo, itemPageRequest = self.spider.fetchData(urlIntern, self.referer)
         itemPagesoup = BeautifulSoup(itemPageRequest)
         block=itemPagesoup.find_all("div", {"class": "productBlockCenter"})
         for item in block:
-            itemurl=item.find("div", {"id": "productTitle"})
+            itemurlinfo=item.find("div", {"id": "productTitle"})
             brand=item.find("div", {"class":"brandTop"})
-            mfr=item.find("li", {"class":"singleBullet"})
+            mfrinfo=item.find("li", {"class":"singleBullet"})
             importer=item.find("div", {"id": "grayMarket"}).text
             LensesNikon=True if brand.text=="Nikon" and  tmp[5]=="Lenses" else False
             if  not re.search('imported',importer, re.IGNORECASE):
-                print importer
-                if mfr!=None:            
-                    mfr= mfr.find("span", {"class": "value"}).text 
-                self.dataItems (itemurl.find('a')['href'],mfr,LensesNikon)
+                if mfrinfo!=None:            
+                    mfr= mfrinfo.find("span", {"class": "value"}).text
+                else:
+                    mfr='' 
+                itemurl=itemurlinfo.find('a')['href']
+                if itemurl:
+                    self.dataItems (itemurl,mfr,LensesNikon)
         if re.search('<a href="[^"]*" class="lnext">Next',itemPageRequest):
             return self.scrapItem(url, pgn + 1)
 
@@ -106,7 +110,7 @@ class Main:
         price_rebate='0'
         date_rebate=''
                     
-
+        print code
         info=dataPagesoup.find("div", {"id": 'productInfo'})
         if re.search('See cart for product details', info.text):
             incar="incar"
@@ -117,8 +121,11 @@ class Main:
                 incar="rebate-incar"
             else:
                 incar="rebate"
-            price_rebate=info.find("li", {"class":"instant hiLight rebates"})
-            price_rebate=abs(float(price_rebate.find("span", {"class": "value red"}).text.strip().replace('$','').replace(',','')))
+            price_rebateInfo=info.find("li", {"class":"instant hiLight rebates"})
+            if price_rebateInfo==None:
+                log.writerow([code,'exception Rebate without value red'])
+                return 0
+            price_rebate=abs(float(price_rebateInfo.find("span", {"class": "value red"}).text.strip().replace('$','').replace(',','')))
              
             date_rebate=info.find("span", {"class": "offerEnds"}).text.strip()#.replace('\S', '')#.replace('\n', '').replace('\r', '')
             match=re.search(r'.......\'..',date_rebate)
@@ -126,9 +133,10 @@ class Main:
                 date_rebate= match.group().replace("'",'').split()
                 date_rebate[0]=monts[date_rebate[0]]
                 date_rebate="20"+date_rebate[2]+"-"+date_rebate[1]+"-"+date_rebate[0]
-        print code
+
         priceinfo=info.find("li", {"class":"price hiLight"})
         if priceinfo==None: 
+            log.writerow([code,'exception any without price hilight'])
             return 0
         classaux=re.search('"*.value.*"',priceinfo.prettify())
 
@@ -136,7 +144,7 @@ class Main:
             classaux=classaux.group().replace("\"",'')
             price=float(priceinfo.find("span", {"class":classaux}).text.strip().replace('$','').replace(',',''))
             if price!= special_price and incar=="normal":
-                incar="incarWithShow"
+                incar=incar+"WithShow"
         
         else:
             price=special_price
@@ -165,14 +173,20 @@ class Main:
                     magentoEspecialPrice=float(magentoEspecialPrice)
                 if magentoPrice:
                     magentoPrice=float(magentoPrice)
+                if incar=="normal" and magentoPrice==price and (magentoEspecialPrice==None or magentoEspecialPrice=='' ):
+                    return 0
+                
+                if LensesNikon:
+                    special_price=price
+                    incar="normal"
+                
                 if price!=magentoPrice or special_price!=magentoEspecialPrice or LensesNikon:
 
                     data=[date.today(),str(mfr),code,name,float(special_price),float(price),incar,price_rebate,date_rebate,magentoEspecialPrice,magentoPrice]
+                    
                     for crumb in breadcrumbs[1:]:
                         data.append(crumb)
-                    if LensesNikon:
-                        data[4]=special_price+100
-                        data.append("BH Special price before"+str(special_price))
+
                     writer.writerow(data)
                     print data                       
         
@@ -184,13 +198,13 @@ class Main:
 if __name__ == "__main__":
     mainUrl = "http://www.bhphotovideo.com"
     monts={"JAN":"1","FEB":"2","MAR":"3","APR":"4","MAY":"5","JUN":"6","JUL":"7","AUG":"8","SEP":"9","OCT":"10","NOV":"11","DEC":"12"}
-    updateArchive=str(date.today())+"_updateTotal.csv"
+    updateArchive=str(date.today())+"_updateTotalSecondRound.csv"
     writer = csv.writer(open(updateArchive, "wb"))
-
+    log = csv.writer(open("log.csv", "wb"))
     config = ConfigParser.ConfigParser()
     config.read("config.ini")
-    #tip="magento_test"
-    tip="magento_live"
+    tip="magento_test"
+    #tip="magento_live"
     mg_url = config.get(tip, "mg_url")
     mg_username = config.get(tip, "mg_username")
     mg_password = config.get(tip, "mg_password") 
